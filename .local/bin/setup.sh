a8e732d8d395a9cb6a359683a41b0f6693121b31
JhonFs <jhonatasade@gmail.com>
2025-12-10 20:46:13 -0300

    Backup Geral: Swaynag, VSCode Wayland, Screenshots Nativos e Scripts atualizados

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -9,21 +9,34 @@ echo -e "${GREEN}### Setup Pós-Instalação: Arch + Sway (Minimal) ###${NC}"
 
 # 1. Lista de Pacotes Nativos (Essenciais + Drivers Genéricos)
 PKGS=(
-    "sway" "swaybg" "swayidle" "swaylock" # Interface
-    "waybar" "mako" "fuzzel" # Utilitários Wayland
-    "grim" "slurp" "wl-clipboard" # Screenshots
-    "foot" "neovim" "nano" "htop" "git" "base-devel" "openssh" # Ferramentas
-    "zsh" "zsh-syntax-highlighting" "zsh-autosuggestions" "starship" # Shell
-    "polkit-gnome" "xdg-desktop-portal-wlr" # Sistema
-    "ttf-jetbrains-mono-nerd" "inter-font" # Fontes
-    "xdg-user-dirs" # Cria pastas Downloads, etc.
-    "flatpak" # Gerenciador de pacotes isolados
-    "mesa" # Gráficos base
-    "libva-intel-driver" # Intel Video (Gen 2-7)
-    "intel-media-driver" # Intel Video (Gen 8+)
-    "vulkan-intel" # Vulkan Intel
-    "man-db" "man-pages" "wl-clipboard" "nodejs" "npm" "unzip"
-    #"gammastep"
+    # 1. Sistema Base, Drivers e Ferramentas de Compilação
+    "base-devel" "git" "openssh" "man-db" "man-pages"
+    "mesa" "libva-intel-driver" "intel-media-driver" "vulkan-intel"
+    "nvidia" "nvidia-utils" "egl-wayland"
+    "polkit-gnome" "xdg-desktop-portal-wlr" "xdg-user-dirs"
+    "nodejs" "npm" "unzip" "flatpak"
+
+    # 2. Interface Gráfica (Sway) e Bloqueio
+    "sway" "swaybg" "swayidle" "swaylock"
+
+    # 3. Shell (Zsh), Terminal e Prompt
+    "zsh" "zsh-syntax-highlighting" "zsh-autosuggestions" "starship"
+    "foot"
+
+    # 4. Utilitários Wayland (Barra, Launcher, Notificações, Clipboard)
+    "waybar" "mako" "fuzzel"
+    "grim" "slurp" "wl-clipboard"
+    # "gammastep"
+
+    # 5. Gerenciadores de Arquivos (GUI + Terminal)
+    "thunar" "thunar-archive-plugin" "thunar-volman" "gvfs" "gvfs-mtp" "file-roller"
+    "yazi" "ffmpegthumbnailer" "unarchiver" "jq" "poppler" "fd" "ripgrep" "fzf" "zoxide"
+
+    # 6. Editores de Texto e Monitoramento
+    "neovim" "nano" "htop"
+
+    # 7. Fontes e Tipografia
+    "ttf-jetbrains-mono-nerd" "inter-font"
 )
 
 echo -e "${GREEN}-> Atualizando e instalando nativos...${NC}"
@@ -32,6 +45,7 @@ sudo pacman -Syu --needed --noconfirm "${PKGS[@]}"
 # 2. Configurar Diretórios de Usuário (Downloads, Documents, etc.)
 echo -e "${GREEN}-> Criando diretórios padrão (XDG)...${NC}"
 xdg-user-dirs-update
+mkdir ~/Imagens/Screenshots
 
 # 3. Instalação do Paru (AUR Helper)
 if ! command -v paru &> /dev/null; then
@@ -46,9 +60,16 @@ else
 fi
 
 # 4. Pacotes AUR
-# Vs Code
-echo -e "${BLUE}-> Instalando VSCode (Binário Oficial)...${NC}"
-paru -S --needed --noconfirm visual-studio-code-bin
+PACOTES=(
+    "visual-studio-code-bin"
+    "gruvbox-material-gtk-theme-git"
+    # "capitaine-cursors-gruvbox"
+    "nwg-look"
+    "capitaine-cursors"
+    # "bibata-cursor-theme"
+    )
+echo -e "${BLUE}-> Instalando Pacotes AUR...${NC}"
+paru -S --needed --noconfirm "${PACOTES[@]}"
 
 # 5. Configurar Flatpak e Instalar Apps
 echo -e "${GREEN}-> Configurando Flatpak...${NC}"
